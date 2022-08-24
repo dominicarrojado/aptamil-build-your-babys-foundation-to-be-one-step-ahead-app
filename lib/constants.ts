277f4f36f5a7f1a379d842b0ecb5fd26bc175e0b
Dominic Arrojado <dominicarrojado@gmail.com>
2022-08-24 22:25:45 +0800

    implement seo tags

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,19 @@
+export const SITE_NAME = 'Dominic Arrojado';
+export const PROJECT_TITLE = 'Singtel Data X Infinity Event Singapore';
+export const MAIN_AUTHOR = 'Dominic Arrojado';
+export const MAIN_TITLE = `${PROJECT_TITLE} | ${SITE_NAME}`;
+export const MAIN_DESC =
+  'Unlock the nutrients needed for Brain Development and Natural Defences! Attractive Prizes up for grabs! A trip to New Zealand worth $5000 & 9 months of Aptamil products worth $2900. Join Now!';
 export const MAIN_ORIGIN = 'https://dominicarrojado.com';
 export const MAIN_PATH =
   '/aptamil-build-your-babys-foundation-to-be-one-step-ahead-app/';
 export const MAIN_URL = `${MAIN_ORIGIN}${MAIN_PATH}`;
+export const META_IMAGE = `${MAIN_URL}images/screenshot.png`;
+export const META_IMAGE_WIDTH = 1197;
+export const META_IMAGE_HEIGHT = 627;
+export const META_IMAGE_ALT =
+  'Unlock the nutrients needed for Brain Development and Natural Defences! Attractive Prizes up for grabs! A trip to New Zealand worth $5000 & 9 months of Aptamil products worth $2900. Join Now!';
+export const META_IMAGE_TYPE = 'image/png';
 
 export const FONTS = [
   'Mairy-Black',
--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import FontFaces from '../components/fontFaces';
+import SeoTags from '../components/seoTags';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <SeoTags />
       <FontFaces />
       <Component {...pageProps} />
     </>
