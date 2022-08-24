69c42b7d31872f7705ad2d7b6e8cb98a22c09581
dominicarrojado <dominicarrojado@gmail.com>
2022-08-24 13:44:37 +0000

    Deploying to gh-pages from @ dominicarrojado/aptamil-build-your-babys-foundation-to-be-one-step-ahead-app@2e0e117384571d742816539965eb73e497d67dee 🚀

---
+++ _next/static/YZBMPZ0DxGmyulfJ_kyzs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0cc9400a7d7596c8.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
